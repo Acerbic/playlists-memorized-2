fa889544ffcb2679065f35ece162ef5d012d4dc4
Anthony Cerbic <kuzzdra@gmail.com>
2019-09-08 21:40:23 +0300

    add Jest testing framework

--- packages/backend/src/handlers/simple.ts
+++ packages/backend/src/handlers/simple.ts
@@ -2,6 +2,6 @@ import { Request, Response } from "express";
 
 export default function(req: Request, res: Response) {
     res.json({
-        hello: "custom!!!!!?",
+        hello: "custom!!!!!",
     });
 }
