0a1661068a8ac418b570751fdf04b8aab496a2a5
Anthony Cerbic <kuzzdra@gmail.com>
2019-11-11 09:54:54 +0300

    fe: basic component connection to redux

--- packages/frontend/components/Header.tsx
+++ packages/frontend/components/Header.tsx
@@ -4,36 +4,32 @@
 
 import React from "react";
 import { Row } from "antd";
-// import { connect, MapStateToProps } from "react-redux";
-// import { AppState } from "../common/redux/store";
+import { connect, MapStateToProps } from "react-redux";
+import { AppState } from "../common/redux/store";
 
-// interface ReduxStateProps {
-//     user: AppState["user"];
-// }
-// const mapStateToProps: MapStateToProps<ReduxStateProps, {}, AppState> = ({
-//     user,
-// }) => {
-//     return {
-//         user,
-//     };
-// };
+interface ReduxStateProps {
+    user: AppState["user"];
+}
+const mapStateToProps: MapStateToProps<ReduxStateProps, {}, AppState> = ({
+    user,
+}) => {
+    return {
+        user,
+    };
+};
 
-// type P = ReduxStateProps & {};
+type P = ReduxStateProps;
 
-// const Header: React.FC<P> = props => {
-const Header: React.FC = props => {
+const Header: React.FC<P> = props => {
     return (
         <Row>
-            {/* {props.user.checking
+            Header
+            {props.user.checking
                 ? "Validating session..."
                 : props.user.info
                 ? "Logged in"
-                : "Logged out"} */}
+                : "Logged out"}
         </Row>
     );
 };
-// export default connect(
-//     mapStateToProps,
-//     null
-// )(Header);
-export default Header;
+export default connect(mapStateToProps)(Header);
